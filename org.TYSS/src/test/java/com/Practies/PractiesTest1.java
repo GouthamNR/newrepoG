a9cd7d8d60c67e85977a2aee0fb63187e4e1f3e3
Goutham <Goutham@DESKTOP-7V04JF6>
2022-07-20 13:17:42 +0530

    updated

--- org.TYSS/src/test/java/com/Practies/PractiesTest1.java
+++ org.TYSS/src/test/java/com/Practies/PractiesTest1.java
@@ -3,17 +3,19 @@ package com.Practies;
 import org.testng.annotations.Test;
 
 public class PractiesTest1 {
-  @Test
-	public static void test() {
-		System.out.println("Test_1 from PractiesTest1");
+    @Test (priority = 1)
+	public static void Test() {
+		System.out.println("Test_1 from practiesTest1 ");
 	}
-  @Test
-  public static void test1() {
-	  System.out.println("Test_2 from PractiesTest1");
-  }
-  
-  @Test
-  public static void test2() {
-	  System.out.println("Test_3 from PractiesTest1");
-  }
+    @Test
+    public static void Test1() {
+    	System.out.println("Test_2 from practiesTest1");
+	    //i don't want to  change anything.
+    	//changed..
+    }
+    
+    @Test
+    public static void Test2() {
+    	System.out.println("Test_3 from practiesTest1");
+    }
 }
---
+++ org.TYSS/src/test/java/com/Practies/PractiesTest3.java
@@ -0,0 +1,21 @@
+package com.Practies;
+
+import org.testng.annotations.Test;
+
+public class PractiesTest3 {
+
+	@Test
+	public static void test() {
+		System.out.println("Test_1 from PractiesTest3");
+	}
+	
+	@Test
+	public static void test1() {
+		System.out.println("Test_2 from PractiesTest3");
+	}
+	
+	@Test
+	public static void test2() {
+		System.out.println("Test_3 from PractiesTest3");
+	}
+}
